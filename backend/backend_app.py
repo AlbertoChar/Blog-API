06ee3a61db4346081be7165b96c82444179b2981
Alberto Charabati <bcharabati@icloud.com>
2023-11-15 19:56:40 +0200

    Added routes for update, delete, search, error handling

--- backend/backend_app.py
+++ backend/backend_app.py
@@ -1,4 +1,4 @@
-from flask import Flask, jsonify
+from flask import Flask, jsonify, request
 from flask_cors import CORS
 
 app = Flask(__name__)
@@ -10,9 +10,109 @@ POSTS = [
 ]
 
 
-@app.route('/api/posts', methods=['GET'])
+def validate_title(new_post):
+    if "title" not in new_post:
+        return False
+    return True
+
+
+def validate_content(new_post):
+    if "content" not in new_post:
+        return False
+    return True
+
+
+@app.route('/api/posts', methods=['GET', 'POST'])
 def get_posts():
+    if request.method == 'POST':
+        new_post = request.get_json()
+        if not validate_title(new_post):
+            return jsonify({"error": "Missing post title"}), 400
+        if not validate_content(new_post):
+            return jsonify({"error": "Missing post content"}), 400
+        new_id = max(post['id'] for post in POSTS) + 1
+        new_post['id'] = new_id
+        POSTS.append(new_post)
+        return jsonify(new_post)
+
     return jsonify(POSTS)
+
+
+def delete_post(id):
+    for post in POSTS:
+        if post['id'] == id:
+            deleted_post = post
+            POSTS.remove(post)
+            return deleted_post
+    return None
+
+
+@app.route('/api/posts/<int:id>', methods=['DELETE'])
+def del_post(id):
+    post = delete_post(id)
+    if post is None:
+        return '', 404
+    return jsonify(post)
+
+
+def update_title(id, updated_post):
+    for post in POSTS:
+        if post['id'] == id:
+            post['title'] = updated_post['title']
+            return post
+    return None
+
+
+def update_content(id, updated_post):
+    for post in POSTS:
+        if post['id'] == id:
+            post['content'] = updated_post['content']
+            return post
+    return None
+
+
+@app.route('/api/posts/<int:id>', methods=['PUT'])
+def update_post(id):
+    updated_post = request.get_json()
+    if validate_title(updated_post):
+        post = update_title(id, updated_post)
+        if post is not None:
+            return jsonify(post)
+        return '', 404
+    if validate_content(updated_post):
+        post = update_content(id, updated_post)
+        if post is not None:
+            return jsonify(post)
+        return '', 404
+
+
+@app.route('/api/posts/search', methods=['GET'])
+def search_post():
+    title_query = request.args.get('title')
+    content_query = request.args.get('content')
+
+    results = []
+    if title_query:
+        for post in POSTS:
+            if title_query.lower() in post['title'].lower():
+                results.append(post)
+    if content_query:
+        for post in POSTS:
+            if content_query.lower() in post['content'].lower():
+                results.append(post)
+    if results:
+        return jsonify(results)
+    return '', 404
+
+
+@app.errorhandler(404)
+def not_found_error(error):
+    return jsonify({"error": "Not Found"}), 404
+
+
+@app.errorhandler(405)
+def method_not_allowed_error(error):
+    return jsonify({"error": "Method Not Allowed"}), 405
 
 
 if __name__ == '__main__':
